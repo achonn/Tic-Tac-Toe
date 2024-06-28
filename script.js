4587061847a52e3ca86a5d89b2dc1b04b51d26a5
Alex Chon <achon@uwaterloo.ca>
2024-06-28 13:13:50 +0900

    Gameboard Module Pattern, Plyer Factory Function Commit

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const board = document.querySelector('#board');
-    const cells = document.querySelectorAll('.cell');
-    let isXTurn = true;
+const GameBoard = (function() {
+    let board = ["", "", "", "", "", "", "", "", ""];
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', () => {
-            if (!cell.classList.contains('x') && !cell.classList.contains('circle')) {
-                cell.classList.add(isXTurn ? 'x' : 'circle');
-                isXTurn = !isXTurn;
-            }
-        })
+    const getBoard = function() {
+        return board;
+    };
 
-        cell.addEventListener('mouseenter', () => {
-            if (!cell.classList.contains('x') && !cell.classList.contains('circle')) {
-                cell.classList.add(isXTurn ? 'hover-x' : 'hover-circle');
-            }
-        })
+    const updateBoard = function(index, marker) {
+        if (board[index] === "") {
+            board[index] = marker;
+            return true;
+        }
+        else return false;
+    };
 
-        cell.addEventListener('mouseleave', () => {
-            cell.classList.remove('hover-x');
-            cell.classList.remove('hover-circle');
-        })
+    const resetBoard = function() {
+        board = ["", "", "", "", "", "", "", "", ""];
+    };
+
+    return {getBoard, updateBoard, resetBoard};
+})();
+
+
+const Player = function(name,marker) {
+    return {name,marker};
+};
+
+
+
+const board = document.querySelector('#board');
+const cells = document.querySelectorAll('.cell');
+let isXTurn = true;
+
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if (!cell.classList.contains('x') && !cell.classList.contains('circle')) {
+            cell.classList.add(isXTurn ? 'x' : 'circle');
+            isXTurn = !isXTurn;
+        }
     })
-})
+
+
+    cell.addEventListener('mouseenter', () => {
+        if (!cell.classList.contains('x') && !cell.classList.contains('circle')) {
+            cell.classList.add(isXTurn ? 'hover-x' : 'hover-circle');
+        }
+    })
+
+
+    cell.addEventListener('mouseleave', () => {
+        cell.classList.remove('hover-x');
+        cell.classList.remove('hover-circle');
+    })
+});
