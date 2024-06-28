9a903e293a548a86c23a599b39aada93525ff5ca
Alex Chon <achon@uwaterloo.ca>
2024-06-28 11:37:33 +0900

    Hover and Click Affects, X Turn Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const board = document.querySelector('#board');
+    const cells = document.querySelectorAll('.cell');
+    let isXTurn = true;
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            if (!cell.classList.contains('x') && !cell.classList.contains('circle')) {
+                cell.classList.add(isXTurn ? 'x' : 'circle');
+                isXTurn = !isXTurn;
+            }
+        })
+
+        cell.addEventListener('mouseenter', () => {
+            if (!cell.classList.contains('x') && !cell.classList.contains('circle')) {
+                cell.classList.add(isXTurn ? 'hover-x' : 'hover-circle');
+            }
+        })
+
+        cell.addEventListener('mouseleave', () => {
+            cell.classList.remove('hover-x');
+            cell.classList.remove('hover-circle');
+        })
+    })
+})
