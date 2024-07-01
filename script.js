1d16bdd1cff759d6c3bd5270ba951978f298aedf
Alex Chon <achon@uwaterloo.ca>
2024-07-01 10:50:54 +0900

    Module Patterns Inital Commit

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
-const GameBoard = (function() {
-    let board = ["", "", "", "", "", "", "", "", ""];
+const Gameboard = (function() {
+    let board = ["", "", "", "", "", "", "", "", "",];
 
-    const getBoard = function() {
-        return board;
-    };
-
-    const updateBoard = function(index, marker) {
-        if (board[index] === "") {
-            board[index] = marker;
-            return true;
-        }
-        else return false;
-    };
+    const getBoard = () => board;
 
     const resetBoard = function() {
-        board = ["", "", "", "", "", "", "", "", ""];
-    };
+        board = ["", "", "", "", "", "", "", "", "",];
+    }
 
-    return {getBoard, updateBoard, resetBoard};
+    return {getBoard, resetBoard};
 })();
 
 
 const Player = function(name,marker) {
     return {name,marker};
-};
+}
 
+
+const GameController = function() {
+    const players = [Player('player 1', 'x'), Player('player 2', 'circle')];
+    let currentPlayerIndex = 0;
+    let isGameOver = false;
+
+    const getCurrentPlayer = players[currentPlayerIndex];
+    const switchPlayer = currentPlayerIndex ? 1 : 0;
+
+    const checkWin = function() {
+        const board = Gameboard.getBoard();
+        const winningCondition = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+       winningCondition.some(condition => {
+        return condition.every(index => {
+            cells[index].classList.contains(currentClass);
+        })
+       })
+        
+    }
+}
 
 
 const board = document.querySelector('#board');
 const cells = document.querySelectorAll('.cell');
 let isXTurn = true;
+const currentClass = isXTurn ? 'x' : 'circle';
 
 cells.forEach(cell => {
     cell.addEventListener('click', () => {
@@ -52,3 +73,11 @@ cells.forEach(cell => {
         cell.classList.remove('hover-circle');
     })
 });
+
+function checkWin(currentClass) {
+    return a.winningCondition.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass)
+        })
+    })
+}
